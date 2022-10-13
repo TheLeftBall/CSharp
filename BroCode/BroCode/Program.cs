91a2c099b7974a59f7db58abc207a446576d6f72
TheLeftBall <eduard.boytsov@gmail.com>
2022-10-13 18:47:48 -0400

    Update Program.cs

--- BroCode/BroCode/Program.cs
+++ BroCode/BroCode/Program.cs
@@ -525,6 +525,17 @@ namespace MyCsProgram
         {
             //while loop = repeats some code while some condition remains true
 
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            while (name == "")
+            {
+                Console.WriteLine("Enter your name: ");
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine("Hello " + name);
+
             Console.ReadKey();
         }
     }
