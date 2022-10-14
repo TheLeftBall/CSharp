af7c9719842030256c1b6f9425472699c17bbe72
TheLeftBall <eduard.boytsov@gmail.com>
2022-10-14 15:58:51 -0400

    Update Program.cs

--- BroCode/BroCode/Program.cs
+++ BroCode/BroCode/Program.cs
@@ -635,6 +635,18 @@ namespace MyCsProgram
             bool playAgain = true;
 
             int min = 1;
+            int max = 100;
+
+            int guess;
+            int number;
+            int guesses;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                number = random.Next(min, max + 1);
+            }
 
             Console.ReadKey();
         }
