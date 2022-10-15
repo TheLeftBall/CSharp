5cbec3b6ac89143431c3ca1b928326fbd393a926
TheLeftBall <eduard.boytsov@gmail.com>
2022-10-15 17:14:41 -0400

    Update Program.cs

--- BroCode/BroCode/Program.cs
+++ BroCode/BroCode/Program.cs
@@ -710,6 +710,24 @@ namespace MyCsProgram
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            bool playAgain = true;
+            string player;
+            string computer;
+
+            while (playAgain)
+            {
+                player = "";
+                computer = "";
+                while(player != "ROCK" && player != "PAPER" && player != "SCISSORS")
+                {
+                    Console.WriteLine("Enter ROCK, PAPER, or SCISSORS: ");
+                    player = Console.ReadLine();
+                    player = player.ToUpper();
+
+                    Console.WriteLine("");
+                }
+            }
 
 
             Console.ReadKey();
