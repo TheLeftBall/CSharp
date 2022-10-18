8a57062496436402129a54fcdacd314ee3e57070
TheLeftBall <eduard.boytsov@gmail.com>
2022-10-18 11:44:57 -0400

    Update Program.cs

--- BroCode/BroCode/Program.cs
+++ BroCode/BroCode/Program.cs
@@ -1320,6 +1320,22 @@ namespace MyCsProgram
             Console.ReadKey();
         }
     }
+
+    class Human
+    {
+        public string name;
+        public int age;
+
+        public void Eat()
+        {
+            Console.WriteLine($"{name} is eating");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine($"{name} is sleeping");
+        }
+    }
 }
 
 //__________________________________
