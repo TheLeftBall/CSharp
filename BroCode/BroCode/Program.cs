8a4f6c425c3dcd3bce290c491365933dcaf0f7c9
TheLeftBall <eduard.boytsov@gmail.com>
2022-10-18 11:47:42 -0400

    sleep

--- BroCode/BroCode/Program.cs
+++ BroCode/BroCode/Program.cs
@@ -1316,12 +1316,20 @@ namespace MyCsProgram
             //         objects can have fields and methods (characteristics & actions)
 
             Human human1 = new Human();
+            Human human2 = new Human();
 
             human1.name = "Rick";
             human1.age = 65;
 
             human1.Eat();
             human1.Sleep();
+
+
+            human2.name = "Morty";
+            human2.age = 14;
+
+            human2.Eat();
+            human2.Sleep();
 
             Console.ReadKey();
         }
